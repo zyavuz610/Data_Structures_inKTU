3ea30416fc049f434d192ec5d5015ec46888f0d4
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2020-10-01 20:35:11 +0300

    Update 01_cf1.1_gpa.py

--- ds_inPython_Book/ch01/01_cf1.1_gpa.py
+++ ds_inPython_Book/ch01/01_cf1.1_gpa.py
@@ -12,8 +12,8 @@ print('Enter a blank line to designate the end.')
 # map from letter grade to point value
 points = {'AA':4.0, 'BA':3.5, 'BB':3.0, 'CB':2.5, 'CC':2.0, 'DC':1.5, 
           'DD':1.0, 'FD':0.5, 'FF':0.0}
-num_courses = 0
-total_points = 0
+num_courses     = 0
+total_points    = 0
 done = False
 while not done:
   grade = input()                          # read line from user
