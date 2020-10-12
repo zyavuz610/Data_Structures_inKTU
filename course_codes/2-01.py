23aac7557b69a92e8e55b04bdcb08bb1b2979d1d
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2020-10-12 18:19:00 +0300

    2. hafta ders sonu

---
+++ course_codes/2-01.py
@@ -0,0 +1,11 @@
+name = "Veri Yapıları"
+print("Merhaba",name,name,sep='\n')
+grade1 = int(input("Arasınav notunu giriniz:"))
+grade2 = input("Final notunuzu giriniz:")
+grade2 = int(grade2)
+
+aver = (grade1 + grade2)/2.0;
+
+print("Ortalama:",aver)
+
+
