82b586485aaa5625dab5624a2fb5a619f9b5c014
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2020-11-03 21:23:39 +0300

    2. ödev dosyaları eklendi

---
+++ projects/20g/hw2/source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void) 
+{
+printf("Data Structures\n");
+int i,n;
+i=0;
+for(i=2;i<5;i++)
+{
+n = i*i;
+if(n%2==0)
+{
+for(int j=0;j<n;j++)
+{
+printf("%d",i);
+}
+printf("%d",n);
+}
+else
+{
+printf("\n");
+}
+}
+return 0;
+}
