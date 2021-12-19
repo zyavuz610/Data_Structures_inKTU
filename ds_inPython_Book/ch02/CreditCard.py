0b49180d9b3ef0b138577c2ceee55a189af23605
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2021-12-19 21:38:08 +0300

    Update CreditCard.py

--- ds_inPython_Book/ch02/CreditCard.py
+++ ds_inPython_Book/ch02/CreditCard.py
@@ -1,84 +1,57 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  5 14:44:46 2020
+import sys                    # sistem modülü  
+from PyQt5.QtCore import *    # Temel pencere kütüphaneleri
+from PyQt5.QtWidgets import *  # görsel kütüphane 
 
-@author: zafer
-"""
-
-
-class CreditCard:                               # class definition    
-  """A consumer credit card."""
-  
-  def __init__(self, customer, bank, acnt, limit):      # constructor
-    """Create a new credit card instance.
-    The initial balance is zero.
-    customer  the name of the customer (e.g., 'John Bowman')
-    bank      the name of the bank (e.g., 'California Savings')
-    acnt      the acount identifier (e.g., '5391 0375 9387 5309')
-    limit     credit limit (measured in dollars)
-    """
-    self._customer = customer       # public variable
-    self._bank = bank               # if "__bank": private variable
-    self._account = acnt
-    self._limit = limit
-    self._balance = 0
-
-  def get_customer(self):
-    """Return name of the customer."""
-    return self._customer
+def window():                                           # pencere oluşturma fonksiyonu
+    app = QApplication(sys.argv)                        # ana uygulamayı oluştur
+    w = QMainWindow()                                   # pencere oluştur
     
-  def get_bank(self):
-    """Return the bank's name."""
-    return self._bank
+    groupBox = QGroupBox(w)                             # groupBox oluştur
+    groupBox.setGeometry(10,10,200,130)                 # konum ve boyutu ayarla
+    groupBox.setTitle("Eğitim İşlemleri")               # groupBox başlığı
+    button1 = QPushButton("Eğitim Verisi Yükle")        # 3 adet buton
+    button2 = QPushButton("Eğitim Parametreleri")       
+    button3 = QPushButton("Sistem Eğitimi")
+    
+    vbox = QVBoxLayout()                                # dikey kutu dizilimi
+    vbox.addWidget(button1)                             # butonları dikey olarak ekle
+    vbox.addWidget(button2)
+    vbox.addWidget(button3)                             # groupBox dizilimini ayarla
+    vbox.addStretch(1)                                  # butonları yayarak yerleştir
+    groupBox.setLayout(vbox)
 
-  def get_account(self):
-    """Return the card identifying number (typically stored as a string)."""
-    return self._account
-
-  def get_limit(self):
-    """Return current credit limit."""
-    return self._limit
-
-  def get_balance(self):
-    """Return current balance."""
-    return self._balance
-
-  def charge(self, price):
-    """Charge given price to the card, assuming sufficient credit limit.
-    Return True if charge was processed; False if charge was denied.
-    """
-    if price + self._balance > self._limit:  # if charge would exceed limit,
-      return False                           # cannot accept charge
-    else:
-      self._balance += price
-      return True
-
-  def make_payment(self, amount):
-    """Process customer payment that reduces balance."""
-    self._balance -= amount
-
-if __name__ == '__main__':
-  wallet = []
-  wallet.append(CreditCard('John Bowman', 'California Savings',
-                           '5391 0375 9387 5309', 2500) )
-  wallet.append(CreditCard('John Bowman', 'California Federal',
-                           '3485 0399 3395 1954', 3500) )
-  wallet.append(CreditCard('John Bowman', 'California Finance',
-                           '5391 0375 9387 5309', 5000) )
-
-  for val in range(1, 17):
-    wallet[0].charge(val)
-    wallet[1].charge(2*val)
-    wallet[2].charge(3*val)
-
-  for c in range(3):
-    print('Customer =', wallet[c].get_customer())
-    print('Bank =', wallet[c].get_bank())
-    print('Account =', wallet[c].get_account())
-    print('Limit =', wallet[c].get_limit())
-    print('Balance =', wallet[c].get_balance())
-    print('Balance2 =', wallet[c]._balance)         # publi variable
-    while wallet[c].get_balance() > 100:
-      wallet[c].make_payment(100)
-      print('New balance =', wallet[c].get_balance())
-    print()
+    groupBox2 = QGroupBox(w)                            # groupBox oluştur
+    groupBox2.setGeometry(10,150,200,160)               # konum ve boyutu ayarla
+    groupBox2.setTitle("Tahmin İşlemleri")              # groupBox başlığı
+    button21 = QPushButton("1.Takım Verisi Yükle")      # 4 adet buton
+    button22 = QPushButton("2.Takım Verisi Yükle")
+    button23 = QPushButton("Tahmin Çalıştır")
+    button24 = QPushButton("Tahmin Sonucu")
+    
+    vbox2 = QVBoxLayout()                               # dikey kutu dizilimi
+    vbox2.addWidget(button21)                           # butonları dikey olarak ekle    
+    vbox2.addWidget(button22)
+    vbox2.addWidget(button23)   
+    vbox2.addWidget(button24)    
+    vbox2.addStretch(1)                                 # butonları yayarak yerleştir.
+    groupBox2.setLayout(vbox2)                          # groupBox dizilimini ayarla
+    
+    groupBox3=QGroupBox(w)                              # groupBox oluştur.
+    groupBox3.setGeometry(10, 320, 200, 90)             # konum ve boyutu ayarla
+    groupBox3.setTitle("Seçenekler")                    # groupBox başlığı
+    button31 = QPushButton("İstatistikler")             # 2 adet buton
+    button32 = QPushButton("Genel Ayarlar")
+    
+    vbox3    = QVBoxLayout()                            # dikey kutu dizilimi
+    vbox3.addWidget(button31)                           # butonları dikey olarak ekle
+    vbox3.addWidget(button32)
+    vbox3.addStretch(1)                                 # butonları yayarak yerleştir.
+    groupBox3.setLayout(vbox3)                          # groupBox dizilimini ayarla
+    
+    w.setGeometry(100,100,800,500)                      # pencere konumu
+    w.setWindowTitle("Futbol Tahmin Uygulaması")        # pencere başlığı
+    w.show()                                            # pencereyi göster
+    sys.exit(app.exec_())                               # uygulamayı çalıştır
+   
+if __name__ == '__main__':                              # bu kod direk çalıştırılırsa
+   window()                                             # pencere fonk. çağır
